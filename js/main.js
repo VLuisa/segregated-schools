752be140aa74d332c8014c9522656f0e97568475
Luisa Vasquez <luna.v97@gmail.com>
2023-12-07 21:15:04 -0500

    scrolling no styling or content

---
+++ js/main.js
@@ -0,0 +1,65 @@
+      // using d3 for convenience
+      var main = d3.select("main");
+      var scrolly = main.select("#scrolly");
+      var figure = scrolly.select("figure");
+      var article = scrolly.select("article");
+      var step = article.selectAll(".step");
+
+      // initialize the scrollama
+      var scroller = scrollama();
+
+      // generic window resize listener event
+      function handleResize() {
+        // 1. update height of step elements
+        var stepH = Math.floor(window.innerHeight * 0.2);
+        step.style("height", stepH + "px");
+
+        var figureHeight = window.innerHeight / 2;
+        var figureMarginTop = (window.innerHeight - figureHeight) / 2;
+
+        figure
+          .style("height", figureHeight + "px")
+          .style("top", figureMarginTop + "px");
+
+        // 3. tell scrollama to update new element dimensions
+        scroller.resize();
+      }
+
+      // scrollama event handlers
+      function handleStepEnter(response) {
+        // console.log(response);
+        // response = { element, direction, index }
+
+        // add color to current step only
+        step.classed("is-active", function (d, i) {
+          return i === response.index;
+        });
+
+        // update graphic based on step
+        if (response.index <= 1) {
+          figure.select("img").attr("src", `img1.png`);
+        } else if (response.index == 2) {
+          figure.select("img").attr("src", `img2.png`);
+        } else if (response.index >= 3) {
+          figure.select("img").attr("src", `img3.png`);
+        }
+      }
+
+      function init() {
+        // 1. force a resize on load to ensure proper dimensions are sent to scrollama
+        handleResize();
+
+        // 2. setup the scroller passing options
+        // 		this will also initialize trigger observations
+        // 3. bind scrollama event handlers (this can be chained like below)
+        scroller
+          .setup({
+            step: "#scrolly article .step",
+            offset: 0.9,
+            debug: false,
+          })
+          .onStepEnter(handleStepEnter);
+      }
+
+      // kick things off
+      init();
